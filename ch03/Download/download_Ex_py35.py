5d025cbe421ff553de6398c43f9834f114502c56
apple.wu <wuziteng2006@163.com>
2017-06-16 22:23:22 +0800

    add comments and worked for python3.5

--- ch03/Download/download_Ex_py35.py
+++ ch03/Download/download_Ex_py35.py
@@ -2,15 +2,15 @@ __author__ = 'applewu'
 
 # -*- encoding:utf8 -*-
 from selenium import webdriver
-import urllib
+import urllib.request
 import os
 
 driver = webdriver.Firefox()
-driver.get('file:///Users/applewu/Documents/iCode/selenium/ch03/Download/download.html')
+driver.get('file:///d:/iCode/iGithub/selenium-exercises/ch03/Download/download.html')
 downloadFile_url = driver.find_element_by_id('fileToDownload').get_attribute('href')
 file_name = 'test.zip'
 # 使用 urllib 模块的方法，将文件下载到本地，命名为 test.zip
-urllib.urlretrieve(downloadFile_url, file_name)
+urllib.request.urlretrieve(downloadFile_url, file_name)
 # 获得当前脚本的所在目录
 current_path = os.path.split(os.path.realpath(__file__))[0]
 # 检查当前目录中是否存在名为 test.zip 的文件
